3f3971ea07043b9c8fc32fcc958647f84f6dcf00
Iranzi-Jean-Damascene <iranzijeand20@gmail.com>
2025-07-15 17:54:57 +0200

    creating navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,20 @@
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img
+          src="public/Logo SMAIT.svg"
+          alt="STUDENTSYNC Logo"
+          className="logo"
+        />
+      </div>
+      <div className="navbar-right">
+        <a href="/" className="nav-link home-link">Home</a>
+        <a href="/add" className="nav-link">Add Student</a>
+        <a href="/students" className="nav-button">All Students</a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
