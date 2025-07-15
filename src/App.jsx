1f7eb04665dfe44b53fc13ee9c120c29461c6804
Sabine Ingabire-test <ingabirezizas@gmail.com>
2025-07-15 19:00:16 +0200

    feat(hello-Section): added  a hero section component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import StudentList from "./pages/StudentList";
 import "./App.css";
+import AddStudent from "./pages/AddStudent";
+import Hello from "./Hello";
 
 const App = () => {
   return (
@@ -10,9 +12,11 @@ const App = () => {
         <Navbar />
         <main className="main-content">
           <Routes>
-            <Route path="/" element={<StudentList />} />
+            <Route path="/AddStudent" element={<StudentList />} />
           </Routes>
         </main>
+        <AddStudent />
+        <Hello />
       </div>
     </Router>
   );
